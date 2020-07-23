89a9957edc0bd4daaa8897b84c26d71c3658b4ac
Cloud User <centos@ip-172-31-53-201.ec2.internal>
2020-07-23 22:10:44 +0000

    template file

--- src/modules/instances/instances.tf
+++ src/modules/instances/instances.tf
@@ -1,4 +1,15 @@
+provider "template"{
 
+}
+
+
+data "template_file" "init" {
+  template = "${file("${path.module}/run-init.sh.tpl")}"
+
+  vars = {
+   vm_role = "webserver"
+  }
+}
 
 
 ################################################  web server #######################################
@@ -8,6 +19,7 @@ instance_type = "t2.micro"
 subnet_id = "${aws_subnet.publicsubnet.id}"
 vpc_security_group_ids = ["${aws_security_group.websg.id}"]
 key_name = "virginia"
+user_data = "${data.template_file.init.rendered}"
 tags = {
 Name = "webserver"
 }
