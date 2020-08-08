13de091bac0aed7752dae3380298d5a6fdd30cc6
krishnamaram2 <48556990+krishnamaram2@users.noreply.github.com>
2020-08-09 01:55:47 +0530

    Update variables.tf

--- src/modules/instances/variables.tf
+++ src/modules/instances/variables.tf
@@ -1,12 +1,12 @@
-variable "myaccesskey"{
-type = string
-}
-variable "mysecretkey"{
-type = "string"
-}
-variable "myregion"{
-type = "string"
-default = "us-east-1"
+#variable "myaccesskey"{
+#type = string
+#}
+#variable "mysecretkey"{
+#type = "string"
+#}
+#variable "myregion"{
+#type = "string"
+#default = "us-east-1"
 }
 variable "myamiid"{
 type = "string"
